d61abeb201d95e65d4dcb5831a030127692f877a
Andrei Burvel <burvelandrei@gmail.com>
2024-01-06 19:02:19 +0300

    v1.1\

--- excersice_8(Shape).py
+++ excersice_8(Shape).py
@@ -7,25 +7,65 @@ class Shape:
     def describe(self):
         print(f"Это геометрическая фигура, цвет - {self.color}.")
 
+
 class Circle(Shape):
     """Класс описывающий окружность."""
-    def __init__(self, color: str, radius:int):
+
+    def __init__(self, color: str, radius: int):
         super().__init__(color)
         self.radius = radius
+
+    def area(self):
+        """Метода для расчёта площади."""
+        return 3.14 * self.radius**2 
 
     def describe(self):
         super().describe()
         print(f"Это окружность. Радиус - {self.radius} см, цвет - {self.color}.")
 
+
 class Rectangle(Shape):
     """Класс описывающий прямоугольник."""
-    pass
+
+    def __init__(self, color: str, length: int, width: int):
+        super().__init__(color)
+        self.length = length
+        self.width = width
+
+    def area(self):
+        """Метода для расчёта площади."""
+        return self.length * self.width
+
+    def describe(self):
+        super().describe()
+        print(f"Это {self.color} прямоугольник. Длина - {self.length} см, ширина - {self.width} см.")
+
 
 class Triangle(Shape):
     """Класс описывающий треугольник."""
-    pass
+    def __init__(self, color: str, base: int, height: int):
+        super().__init__(color)
+        self.base = base
+        self.height = height
+
+    def area(self):
+        """Метода для расчёта площади."""
+        return 0.5 * self.base * self.height
+
+    def describe(self):
+        super().describe()
+        print(f"Это {self.color} треугольник с основанием {self.base} и высотой {self.height} см.")
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    #Создадим экземпляры каждой фигуры.
     circle = Circle("красный", 5)
+    rectangle = Rectangle("синий", 3, 4)
+    triangle = Triangle("фиолетовый", 6, 8)
+    #Вызовем метод описания для каждого экземпляра.
     circle.describe()
+    rectangle.describe()
+    triangle.describe()
+    #Выведем площадь для каждого экземпляра
+    print(f"Площадь треугольника {triangle.area()}, окружности {circle.area()}, прямоугольника {rectangle.area()} см.")
